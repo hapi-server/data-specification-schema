90ea37a4ddbe57818f8835ff835c055e968a7107
Bob Weigel <rweigel@gmu.edu>
2023-12-29 12:54:20 -0500

    tests for 3.0

---
+++ test/3.0/get.sh
@@ -0,0 +1,3 @@
+
+curl "https://hapi-server.org/servers-dev/TestData3.0/hapi/info?id=dataset1&resolve_references=false" > TestData3.0-dataset1.json
+curl "https://hapi-server.org/servers-dev/TestData3.0/hapi/info?id=dataset1" > TestData3.0-dataset1-resolved.json
---
+++ test/3.0/test.sh
@@ -0,0 +1,3 @@
+node ../../../verifier-nodejs/validate.js refs.json
+node ../../../verifier-nodejs/validate.js TestData3.0-dataset1.json
+node ../../../verifier-nodejs/validate.js TestData3.0-dataset1-resolved.json
