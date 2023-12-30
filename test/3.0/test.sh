6ada55a9064a90f3f7db8bd8b60f084e9d896a1f
Bob Weigel <rweigel@gmu.edu>
2023-12-29 20:45:04 -0500

    add /about test

--- test/3.0/test.sh
+++ test/3.0/test.sh
@@ -1,3 +1,5 @@
-node ../../../verifier-nodejs/validate.js refs.json
-node ../../../verifier-nodejs/validate.js TestData3.0-dataset1.json
-node ../../../verifier-nodejs/validate.js TestData3.0-dataset1-resolved.json
+VADLIDATE=../../../verifier-nodejs/validate.js
+node $VADLIDATE refs.json
+node $VADLIDATE TestData3.0-dataset1.json
+node $VADLIDATE TestData3.0-dataset1-resolved.json
+node $VADLIDATE TestData3.0-about.json
