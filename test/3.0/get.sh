c824a40c48cecdb5a52adedb485191f910a5a855
Bob Weigel <rweigel@gmu.edu>
2024-01-16 20:42:33 -0500

    change URL in 3.0/get.sh

--- test/3.0/get.sh
+++ test/3.0/get.sh
@@ -1,4 +1,4 @@
 
-curl "https://hapi-server.org/servers-dev/TestData3.0/hapi/info?id=dataset1&resolve_references=false" > TestData3.0-dataset1.json
-curl "https://hapi-server.org/servers-dev/TestData3.0/hapi/info?id=dataset1" > TestData3.0-dataset1-resolved.json
-curl "http://localhost:8998/TestData3.0/hapi/about" > TestData3.0-about.json
+curl "https://hapi-server.org/servers/TestData3.0/hapi/info?id=dataset1&resolve_references=false" > TestData3.0-dataset1.json
+curl "https://hapi-server.org/servers/TestData3.0/hapi/info?id=dataset1" > TestData3.0-dataset1-resolved.json
+curl "https://hapi-server.org/servers/TestData3.0/hapi/about" > TestData3.0-about.json
