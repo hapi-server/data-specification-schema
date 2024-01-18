25fc580cec828a98c65c52b2288c6203e8ab00e6
Bob Weigel <rweigel@gmu.edu>
2024-01-18 08:39:05 -0500

    demo issue #11

--- test/3.0/test.sh
+++ test/3.0/test.sh
@@ -1,4 +1,5 @@
 VADLIDATE=../node_modules/hapi-server-verifier/validate.js
+node $VADLIDATE error.json
 node $VADLIDATE refs.json
 node $VADLIDATE TestData3.0-dataset1.json
 node $VADLIDATE TestData3.0-dataset1-resolved.json
