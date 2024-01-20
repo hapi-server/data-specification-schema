67f050407b19b6e622e1bb73acd0bf80661dde67
Bob Weigel <rweigel@gmu.edu>
2024-01-19 19:35:38 -0500

    log test results

--- test/3.1/test.sh
+++ test/3.1/test.sh
@@ -1,5 +1,5 @@
 VADLIDATE=../node_modules/hapi-server-verifier/validate.js
-node $VADLIDATE refs.json
-node $VADLIDATE TestData3.1-dataset1.json
-node $VADLIDATE TestData3.1-dataset1-Aα☃.json
-node $VADLIDATE TestData3.1-dataset2.json
+node $VADLIDATE refs.json | tee refs.json.log
+node $VADLIDATE TestData3.1-dataset1.json | tee TestData3.1-dataset1.json.log
+node $VADLIDATE TestData3.1-dataset1-Aα☃.json | tee TestData3.1-dataset1-Aα☃.json.log
+node $VADLIDATE TestData3.1-dataset2.json | tee TestData3.1-dataset2.json.log
--- test/3.2/test.sh
+++ test/3.2/test.sh
@@ -1,2 +1,2 @@
 VADLIDATE=../node_modules/hapi-server-verifier/validate.js
-node $VADLIDATE TestData3.2-catalog-all.json
+node $VADLIDATE TestData3.2-catalog-all.json | tee TestData3.2-catalog-all.json.log
