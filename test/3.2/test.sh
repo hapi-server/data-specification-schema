b743a763020afbc9d388b5225ba04b262c46d6c3
Bob Weigel <rweigel@gmu.edu>
2024-02-09 15:47:05 -0500

    3.2 stringType

--- test/3.2/test.sh
+++ test/3.2/test.sh
@@ -1,2 +1,3 @@
 VADLIDATE=../node_modules/hapi-server-verifier/validate.js
 node $VADLIDATE TestData3.2-catalog-all.json | tee TestData3.2-catalog-all.json.log
+node $VADLIDATE TestData3.2-catalog-all-stringType.json | tee TestData3.2-catalog-all-stringType.json.log
