28bae92e4dc0b2d8a281b57160497f0b4711bc89
Bob Weigel <rweigel@gmu.edu>
2024-02-09 16:32:58 -0500

    Fix typo in test.sh scripts

--- test/3.0/test.sh
+++ test/3.0/test.sh
@@ -1,6 +1,6 @@
-VADLIDATE=../node_modules/hapi-server-verifier/validate.js
-node $VADLIDATE error.json | tee error.json.log
-node $VADLIDATE refs.json | tee refs.json.log
-node $VADLIDATE TestData3.0-dataset1.json | tee TestData3.0-dataset1.json.log
-node $VADLIDATE TestData3.0-dataset1-resolved.json | tee TestData3.0-dataset1-resolved.json.log
-node $VADLIDATE TestData3.0-about.json | tee TestData3.0-about.json.log
+VALIDATE=../node_modules/hapi-server-verifier/validate.js
+node $VALIDATE error.json | tee error.json.log
+node $VALIDATE refs.json | tee refs.json.log
+node $VALIDATE TestData3.0-dataset1.json | tee TestData3.0-dataset1.json.log
+node $VALIDATE TestData3.0-dataset1-resolved.json | tee TestData3.0-dataset1-resolved.json.log
+node $VALIDATE TestData3.0-about.json | tee TestData3.0-about.json.log
--- test/3.2/test.sh
+++ test/3.2/test.sh
@@ -1,3 +1,4 @@
-VADLIDATE=../node_modules/hapi-server-verifier/validate.js
-node $VADLIDATE TestData3.2-catalog-all.json | tee TestData3.2-catalog-all.json.log
-node $VADLIDATE TestData3.2-catalog-all-stringType.json | tee TestData3.2-catalog-all-stringType.json.log
+VALIDATE=../node_modules/hapi-server-verifier/validate.js
+node $VALIDATE TestData3.2-catalog-all.json | tee TestData3.2-catalog-all.json.log
+node $VALIDATE TestData3.2-catalog-all-stringType.json | tee TestData3.2-catalog-all-stringType.json.log
+node $VALIDATE TestData3.2-about.json | tee TestData3.2-about.json.log
