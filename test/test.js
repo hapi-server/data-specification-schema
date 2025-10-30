6cd1df33ca07210169f6b213db2133abf845bc27
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-10-30 17:32:53 -0400

    logging

--- test/test.js
+++ test/test.js
@@ -17,11 +17,24 @@ files.forEach(file => {
   const logFile = `${file}.log`
   const command = `node ${VALIDATE} ${file}`
   exec(command, (err, stdout, stderr) => {
-    if (err) {
+    if (err && stderr.trim()) {
+      // If validation fails, validate.js exits with code 1 and no stderr.
+      // If stderr exists, it's an execution error.
       console.error(`Error executing command for ${file}:`, err)
       return
     }
     fs.writeFileSync(logFile, stdout)
+
+    if (typeof stdout === 'string') {
+
+      const RED = '\x1b[41m'
+      const RESET = '\x1b[0m'
+      stdout = stdout.replace(/✗/g, `${RED}✗${RESET}`)
+      const GREEN = '\x1b[42m'
+      stdout = stdout.replace(/✓/g, `${GREEN}✓${RESET}`)
+      const YELLOW = '\x1b[43m'
+      stdout = stdout.replace(/⚠/g, `${YELLOW}⚠${RESET}`)
+    }
     console.log(stdout)
     if (stderr.trim()) {
       console.log(stderr)
